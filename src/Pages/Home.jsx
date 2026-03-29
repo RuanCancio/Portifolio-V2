6a5f709a04a42a6ec3d2c872c27fc2b6e0afba1e
Ruan Cancio <slidruan@gmail.com>
2026-03-29 00:09:47 -0300

    Media Query atualizada!

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import Ruan from "../assets/1080.jpg"
+import Ruan from "../assets/icon_me.jpg"
 import TypingInput from "../components/TypingInput.jsx"
 import Styles from "../Css/Layout.module.css"
 import GitIcon from "../assets/Github.svg"
@@ -24,7 +24,7 @@ const Home = () => {
                         <a href="https://www.instagram.com/cancioruan/" target="_blank"><img src={InstaIcon} alt="Meu Instagram" /></a>
                         <a href="https://www.linkedin.com/in/ruancancio/" target="_blank"><img src={LinkedIcon} alt="Meu LinkedIn" /></a>
                     </div>
-                    <div className={Styles.btn_cv}><a href="/assets/Ruan_Cancio_Desenvolvedor_Junior.pdf" download>Baixar Currículo</a></div>
+                    <div className={Styles.btn_cv}><a href="/assets/Ruan_Barata_Cancio_Desenvolvedor_FullStack.pdf" download>Baixar Currículo</a></div>
                 </section>
             </Reveal>
             <Reveal>
